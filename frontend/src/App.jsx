1e1bf161701894869749ab572b398f7131822f68
ilhadonorte <ilhadonorte@gmail.com>
2024-12-18 15:27:20 -0300

    начинаю потихоньку изучать работу с модальными окнами

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,29 @@ import './App.css'
 import ParticleCard from './components/PacticleCard';
 import ParticlesGroup from './components/ParticlesGroup';
 import { API_PARTICLES_URL, API_NAMES_URL } from "./constants"; 
+import ReactModal from 'react-modal';
+import { SimpleModal } from "./components/SimpleModal/SimpleModal";
+import axios from "axios";
 
 function App() {
   const [count, setCount] = useState(0)
   const [particles, setParticles] = useState([])
   const [leptons, setLeptons] = useState([])
+  const [modalForEditNameIsOpen, setModalForEditNameIsOpen] = useState(false);
+  const [formData, setFormData] = useState({
+    baseid:"",
+    name_ru:"",
+    name_pt:""
+  })
+  // const axios = require('axios');
 
   useEffect(() => {
     fetchParticles();
   }, []);  
 
   const fetchParticles = async () => {
+    console.time('fetchParticles');
+
     try {
       const response = await fetch(API_PARTICLES_URL);
       const particles = await response.json();
@@ -34,13 +46,101 @@ function App() {
     } catch (err) {
       console.log(err);
     }
+    console.timeEnd('fetchParticles');
   };  
 
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+  
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const modalContent = (
+    <div>
+      <h2>Заголовок модального окна</h2>
+      <p>Текст модального окна</p>
+      <button onClick={closeModal}>Закрыть</button>
+    </div>
+  );
+
+const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+async function editParticleName()
+  {
+    axios.post({API_NAMES_URL}, 
+      {
+      firstName: 'Fred',
+      lastName: 'Flintstone'
+      })
+    .then(function (response) 
+      {
+        console.log(response);
+      })
+    .catch(function (error) 
+      {
+        console.log(error);
+      })
+    .finally(function () 
+      {
+        // выполняется всегда
+      });
+  
+  }
 
 
   return (
     <>
       <div>
+        {/* <ReactModal 
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+    >
+          React modal window here
+          {modalContent}
+        </ReactModal> */}
+        
+        <SimpleModal
+          isOpen={modalForEditNameIsOpen}    
+          onClose={() => setModalForEditNameIsOpen(false)}
+        >
+          <h2>Add/edit particle name..</h2>
+          <p>
+
+        <input
+          type="text"
+          name="baseid"
+          value={formData.baseid}
+          placeholder="Particle baseid..."
+          onChange={handleChange}
+        /> 
+        <br></br>
+        <input
+          type="text"
+          name="name_ru"
+          value={formData.name_ru}
+          placeholder="Russian name..."
+          onChange={handleChange}
+        />
+        <br></br>
+        <input
+          type="text"
+          name="name_pt"
+          value={formData.name_pt}
+          placeholder="Português name..."
+          onChange={handleChange}
+        />
+        <br></br>
+        <button >Save name </button>
+          </p>
+        </SimpleModal>
+      
       <h1>Стандартная модель элементарных частиц </h1>
       Добавить селекторы для языка
       <a href="https://pdg.lbl.gov/2024/api/index.html" target='blank'> pdg group</a> | 
@@ -55,8 +155,15 @@ function App() {
       </div>
       
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => {count + 1; setModalIsOpen(!modalIsOpen)})}>
           count is {count}
+        </button>
+
+        <button
+          className="modal-show-button"
+          onClick={() => setModalForEditNameIsOpen(true)}
+        >
+          Add/edit particle name
         </button>
       </div>
 
--- frontend/src/components/PacticleCard.jsx
+++ frontend/src/components/PacticleCard.jsx
@@ -8,7 +8,7 @@ export default function ParticleCard({particle})
     else if (particle.is_baryon) { backgroundColor = "#138D75"}
     else if (particle.is_lepton) { backgroundColor = "#00FF00"}
     else if (particle.is_meson) { backgroundColor = "#FF5733"}
-    else if (particle.is_quark) { backgroundColor = "#DD33FF"}
+    else if (particle.is_quark) { backgroundColor = "#bb8fce"}
     else {backgroundColor = "#78281F"}
     // console.log("backgroundColor:", backgroundColor, particle.number)
     return(
